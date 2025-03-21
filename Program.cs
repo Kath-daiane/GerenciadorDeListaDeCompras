19720c3f7f15b3127a119f4898989511e662a4e0
Kath-daiane <kath.daiane.p@gmail.com>
2025-03-21 11:15:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Lista de compras
+        List<string> listaCompras = new List<string>();
+
+        // Adicionando 5 itens à lista
+        Console.Write("Digite o nome do item a adicionar: ");
+        string item1 = Console.ReadLine();
+        listaCompras.Add(item1);
+        Console.WriteLine($"Item '{item1}' adicionado à lista.");
+
+        Console.Write("Digite o nome do item a adicionar: ");
+        string item2 = Console.ReadLine();
+        listaCompras.Add(item2);
+        Console.WriteLine($"Item '{item2}' adicionado à lista.");
+
+        Console.Write("Digite o nome do item a adicionar: ");
+        string item3 = Console.ReadLine();
+        listaCompras.Add(item3);
+        Console.WriteLine($"Item '{item3}' adicionado à lista.");
+
+        Console.Write("Digite o nome do item a adicionar: ");
+        string item4 = Console.ReadLine();
+        listaCompras.Add(item4);
+        Console.WriteLine($"Item '{item4}' adicionado à lista.");
+
+        Console.Write("Digite o nome do item a adicionar: ");
+        string item5 = Console.ReadLine();
+        listaCompras.Add(item5);
+        Console.WriteLine($"Item '{item5}' adicionado à lista.");
+
+        // Exibir todos os itens na lista
+        Console.WriteLine("\nItens na lista de compras:");
+        foreach (var item in listaCompras)
+        {
+            Console.WriteLine($"- {item}");
+        }
+
+        // Remover um item específico
+        Console.Write("\nDigite o nome do item a remover: ");
+        string itemRemover = Console.ReadLine();
+
+        if (listaCompras.Contains(itemRemover))
+        {
+            listaCompras.Remove(itemRemover);
+            Console.WriteLine($"Item '{itemRemover}' removido da lista.");
+        }
+        else
+        {
+            Console.WriteLine($"Item '{itemRemover}' não encontrado na lista.");
+        }
+
+        // Exibir itens restantes na lista
+        Console.WriteLine("\nItens restantes na lista de compras:");
+        if (listaCompras.Count > 0)
+        {
+            foreach (var item in listaCompras)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("A lista está vazia.");
+        }
+    }
+}
